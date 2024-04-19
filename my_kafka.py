96b0b8a58fa05d6010b4875a247c8295de289016
GDIATTA <gaussjatta1@outlook.fr>
2024-04-19 13:40:47 +0200

    some files

---
+++ my_kafka.py
@@ -0,0 +1,13 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+
+topic_name='hello_world'
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+for e in range(1000):
+    data = {'number':e}
+    print(data)
+    producer.send(topic_name, value=data)
+    sleep(5)
